319bda6fe22453016af696355a5453bf32e493b6
Pelin Sari <begonyas@yahoo.com>
2024-08-31 19:17:08 +0200

    Add files via upload

---
+++ pyvis-deneme.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import dash
+from dash import html
+from dash.dependencies import Input, Output
+import pyvis
+from pyvis.network import Network
+
+# Create Pyvis Network graph
+net = Network(notebook=True, cdn_resources='remote')
+net.add_node(1)
+net.add_node(2)
+net.add_edge(1, 2)
+net_html = net.show("graph.html")
+
+# Create Dash app
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    html.Iframe(srcDoc=open('graph.html', 'r').read(), style={'width': '100%', 'height': '600px'})
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
+# In[ ]:
+
+
+
+

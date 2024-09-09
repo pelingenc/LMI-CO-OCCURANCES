c1aa154ae92b1b7fb006fd2e4c4fa12e2dd46edc
Pelin Sari <begonyas@yahoo.com>
2024-09-09 15:00:43 +0200

    Update pyvis-deneme.py

--- pyvis-deneme.py
+++ pyvis-deneme.py
@@ -845,7 +845,7 @@ def update_charts(selected_code, show_labels, slider_value, data):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8051)
+    app.run_server(debug=True, port=8052)
 
 
 # In[ ]:
